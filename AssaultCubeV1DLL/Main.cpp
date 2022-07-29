e45d966115470b5ca0b861733dcb4391aecd920b
Ism1tha <xgalan@hotmail.com>
2022-07-29 12:44:01 +0200

    Initial commit

---
+++ AssaultCubeV1DLL/Main.cpp
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_DEPRECATE
+
+#include <stdio.h>
+#include <Windows.h>
+#include <iostream>
+
+void showInjectionStatus();
+
+DWORD moduleBase;
+DWORD localPlayer;
+int health;
+
+BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpReserved)
+{
+    if (fdwReason == DLL_PROCESS_ATTACH) 
+    {
+        DWORD moduleBase = (DWORD)GetModuleHandle(L"ac_client.exe");
+        DWORD localPlayer = *(DWORD*)(moduleBase + 0x18AC00);
+        DWORD playerHealth = *(int*)(localPlayer + 0xEC);
+        AllocConsole();
+        freopen("CONOUT$", "w", stdout);
+        std::cout << "moduleBase: " << moduleBase << "\n";
+        std::cout << "localPlayer: " << localPlayer << "\n";
+        std::cout << "playerHealth: " << playerHealth << "\n";
+
+        *(DWORD*)(localPlayer + 0xEC) = 1337;
+    }
+    return true;
+}
